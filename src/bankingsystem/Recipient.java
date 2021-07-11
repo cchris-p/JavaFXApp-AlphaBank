be7c7a9aa63ec4748ca07c7c972ae57a872c814a
xh88 <rarityrings333@gmail.com>
2021-07-11 07:36:45 -0400

    Initial commit

---
+++ src/bankingsystem/Recipient.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankingsystem;
+
+/**
+ *
+ * @author pyles
+ */
+public class Recipient {
+    public String name;
+    public int accountNumber;
+    public String dateAdded;
+}
